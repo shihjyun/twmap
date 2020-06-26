c444e2e5b56be14a9ad49328b2c2a0e80c9accac
shihjyun <shihjyun.yeo@gmail.com>
2020-06-26 21:19:02 +0800

    add adjusted Taiwan county/town geometry data - remove uninhabited island - reduce the distance between other island and Taiwan - reduce the size of geometry file

---
+++ R/tw_county_adj.R
@@ -0,0 +1,11 @@
+#' Taiwan county map produced by NLSC(EPSG:3824, TWD97)
+#'
+#' Last update: 2019-11-21
+#'
+#' Adjusted Part:
+#' - remove uninhabited island
+#' - reduce the distance between other island and Taiwan
+#' - reduce the size of geometry file
+#'
+#' @source \url{https://data.gov.tw/dataset/7442}
+"tw_county_adj"
---
+++ R/tw_town_adj.R
@@ -0,0 +1,11 @@
+#' Taiwan town map produced by NLSC(EPSG:3824, TWD97)
+#'
+#' Last update: 2020-03-24
+#'
+#' Adjusted Part:
+#' - remove uninhabited island
+#' - reduce the distance between other island and Taiwan
+#' - reduce the size of geometry file
+#'
+#' @source \url{https://data.gov.tw/dataset/7441}
+"tw_town_adj"
